fb8432f5f57f8d4fb6c667ce48f568754d21dbe2
paulmboyce <paul.boyce@goobito.com>
2020-04-15 19:53:17 -0300

    Added supertest package for endpont Server tests.  Example test in io.test.js

--- routes/io.test.js
+++ routes/io.test.js
@@ -1,3 +1,7 @@
+const app = require('../endpoint'); // Link to your server file
+const supertest = require('supertest');
+const request = supertest(app);
+
 // This passes because 1 === 1
 it('Testing to see if Jest works', () => {
     expect(1).toBe(1);
@@ -8,5 +12,28 @@ it('Async test', async () => {
     // Do your async tests here
         expect(2).toBe(2);
         resolve();
+    });
+});
+
+/*
+it('Gets the test endpoint', async (done) => {
+    try {
+        // Sends GET Request to /test endpoint
+        const res = await request.get('/io/summary');
+        console.log(res.body);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ oneWeek: 83, twoWeeks: 348 });
+        done();
+    } catch (err) {
+        done('ERROR: ' + err);
+    }
+});
+*/
+
+it('Gets the test endpoint (Async, returns a Promise).', async () => {
+    // Sends GET Request to /test endpoint
+    return request.get('/io/summary').then(res => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ oneWeek: 83, twoWeeks: 348 });
     });
 });
