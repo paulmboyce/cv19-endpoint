90408bb0c13ec5995b3034a55f543d8d99424a66
paulmboyce <paul.boyce@goobito.com>
2020-04-15 17:45:25 -0300

    Added ESLint for code formating. Ran ESLint to baseline .js files

--- routes/io.js
+++ routes/io.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* Get IO . */
-router.get('/summary', function(req, res, next) {
-  res.json({oneWeek: 83, twoWeeks: 348});
+router.get('/summary', function (req, res, next) {
+    res.json({ oneWeek: 83, twoWeeks: 348 });
 });
 
 module.exports = router;
--- routes/io.test.js
+++ routes/io.test.js
@@ -1,12 +1,12 @@
 // This passes because 1 === 1
 it('Testing to see if Jest works', () => {
     expect(1).toBe(1);
-  })
+});
 
-
-it('Async test', async done => {
+it('Async test', async () => {
+    return new Promise(resolve => {
     // Do your async tests here
-    expect(2).toBe(2);
-    
-    done()
-  })
+        expect(2).toBe(2);
+        resolve();
+    });
+});
